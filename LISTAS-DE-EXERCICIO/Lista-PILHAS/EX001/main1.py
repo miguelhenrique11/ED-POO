1cf669b71797d7fbe1ad42c7d96924112f3266b3
Miguel Henrique Pereira Marculino <20192380037@ad.ifpb.edu.br>
2020-03-16 09:21:41 -0300

    16/06/2019

--- LISTAS-DE-EXERCICIO/Lista-PILHAS/EX001/main1.py
+++ LISTAS-DE-EXERCICIO/Lista-PILHAS/EX001/main1.py
@@ -1,11 +1,48 @@
 from class1 import Pilha
 def line():
-    print("==========================================================")
+    return("==========================================================")
 
-def menu(opt):
-    print()
+
+
+def pulaLinnha(num):
+    for i in range(num):
+        print()
+    return(line())
+
+def manual():
+    line()
+    return("MANUAL DE COMANDOS"
+           "\n=========================================================="
+           "\n # - APAGA O ULTIMO DIGITO"
+           "\n @ - APAGA TODO O TEXTO"
+           "\n !! - ENCERRA O PROGRAMA")
+
 
 def main():
+    print(line())
+    print("EDITOR")
+    print(pulaLinnha(1))
+    print(manual())
+    print(pulaLinnha(1))
+
     pilha=Pilha()
     executando=True
     while(executando):
+        dgt=input()
+        if(dgt=="#"):
+            if(pilha.size()>0):
+                pilha.pop()
+        elif(dgt=="@"):
+            if(pilha.size()>0):
+                pilha.popAll()
+        elif(dgt=="!!"):
+            print(pulaLinnha(4))
+            print("FIM DO PROGRAMA")
+            print(line())
+            print("SEU TEXTO")
+            executando=False
+        else:
+            pilha.push(dgt)
+        print(pilha.getPilha())
+        print(pulaLinnha(4))
+main()
